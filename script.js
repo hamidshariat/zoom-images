26642d18a6bbe8131a69bd85c2a1276090dc70d3
hamidshariat <hamid13841147@gmail.com>
2023-10-13 12:33:07 +0330

    Modify the code

--- script.js
+++ script.js
@@ -2,21 +2,27 @@
     let defineLibrary = () => ({
         init : function(galleryId) {
             let container = document.querySelector(galleryId);
+
             if(! container) {
                 console.error('please add the correct element')
                 return;
             }
+
             let firstImage = container.querySelector('.small-preview')
             let zoomedImage = container.querySelector('.zoomed-image');
+
             if(! zoomedImage) {
                 console.error('please add a .zoomed-image tag');
                 return;
             }
+
             if(! firstImage) {
                 console.log('please add images with .small-preview class to your gallery');
                 return;
             }
+
             zoomedImage.style.backgroundImage = `url(${firstImage.src})`
+
             container.addEventListener('click' , function(e) {
                 let elem = e.target;
 
@@ -24,9 +30,11 @@
                     zoomedImage.style.backgroundImage = `url(${elem.src})`
                 }
             })
+
             zoomedImage.addEventListener('mouseenter' , function (){
                 this.style.backgroundSize = '200%'
             })
+
             zoomedImage.addEventListener('mousemove' , function(e) {
                 let dimentions = this.getBoundingClientRect()
                 let x = e.clientX - dimentions.left
@@ -35,15 +43,17 @@
                 y = Math.round(100/(dimentions.height / y));
                 this.style.backgroundPosition = `${x}% ${y}%`
             })
+
             zoomedImage.addEventListener('mouseleave' , function() {
                 this.style.backgroundSize = 'cover'
                 this.style.backgroundPosition = 'center'
             })
+
         }
     })
+
     if(typeof(vanillaZoom) == 'undefined') {
         window.vanillaZoom = defineLibrary();
-    } else {
-        console.log('library already defined.')
     }
-})(window)
+})
+(window)
